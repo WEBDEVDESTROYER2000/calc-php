84b659151d4859ed86835f05376a2309ecbae55a
WEBDEVDESTROYER2000 <idiotmailservice@gmail.com>
2026-07-01 16:04:14 +0400

    Update title and form handling in index.php
    Recode

--- index.php
+++ index.php
@@ -1,87 +1,91 @@
-<!DOCTYPE HTML>
 <html>
     <head>
-        <title>placeholder</title>
+        <title>Calculator RECODE v0.2</title>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <!--The line above is for a failed favicon feature; don't mind it.-->
     </head>
     <body>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
             <input required type="number" name="num1" placeholder="FIRST_NUMBER">
-            <select name="op" id="op">
-                <option value="add">+</option>
-                <option value="subtract">-</option>
-                <option value="multiply">*</option>
-                <option value="divide">/</option>
-                <option value="modulate">%</option>
-                <option value="exponentiate">**</option>
-                <option value="absolute-add">|+|</option>
-                <option value="absolute-subtract">|-|</option>
-                <option value="radical">√</option>
-                <option value="random">?</option>
+            <select name="specops">
+                <option value="sum">+</option>
+                <option value="diff">-</option>
+                <option value="prd">*</option>
+                <option value="div">/</option>
+                <option value="mod">%</option>
+                <option value="exp">**</option>
+                <option value="abs-sum">|+|</option>
+                <option value="abs-diff">|-|</option>
+                <option value="rad">√</option>
+                <option value="prob">?</option>
             </select>
             <input required type="number" name="num2" placeholder="SECOND_NUMBER"><br>
-            <button type="submit">Calculate</button>
+            <button>DO THE MATHS</button>
         </form>
         <?php
         if($_SERVER["REQUEST_METHOD"]=="POST"){
-            $bug=false;
-            $num1=filter_input(INPUT_POST, "num1",FILTER_SANITIZE_NUMBER_FLOAT);
-            $num2=filter_input(INPUT_POST, "num2",FILTER_SANITIZE_NUMBER_FLOAT);
-            $operator=htmlspecialchars($_POST["op"]);
+            $num1=filter_input(INPUT_POST, "num1", FILTER_SANITIZE_NUMBER_FLOAT);
+            $num2=filter_input(INPUT_POST, "num2", FILTER_SANITIZE_NUMBER_FLOAT);
+            $ops=htmlspecialchars($_POST["specops"]);
+            $breach=false;
+            if(empty($num1)||empty($num2)||empty($ops)){
+                echo "OH NONONNONONO WHAT IS THIS?";
+                $breach=true;
+            };
             if(!is_numeric($num1)||!is_numeric($num2)){
-            $bug=true;
-            echo "YOU SUCK";
-        }
-            if(empty($num1)||empty($num2)){
-            $bug=true;
-            echo "ACCESS DENIED";
-        }
-            if(empty($num1));
-            switch($operator){;
-                case "add":
-                    $value=$num1 + $num2;
-                    echo $value;
-                    break;
-                case "subtract":
-                    $value=$num1 - $num2;
-                    echo $value;
-                    break;
-                case "multiply":
-                    $value=$num1*$num2;
-                    echo $value;
-                    break;
-                case "divide":
-                    $value=$num1/$num2;
-                    echo $value;
-                    break;
-                case "modulate":
-                    $value=$num1%$num2;
-                    echo $value;
-                    break;
-                case "exponentiate":
-                    $value=pow($num1,$num2);
-                    echo $value;
-                    break;
-                case "absolute-add":
-                    $value=abs($num1+$num2);
-                    echo "|",$value,"|";
-                    break;
-                case "absolute-subtract":
-                    $value=abs($num1-$num2);
-                    echo "|",$value,"|";
-                    break;
-                case "radical":
-                    $value=$num1*sqrt($num2);
-                    echo $value;
-                    break;
-                case "random":
-                    $value=rand($num1,$num2);
-                    echo $value;
-                    break;
-                default:
-                    echo "FAIL!";
-            }
+                echo "ACCESS DENIED";
+                $breach=true;
+            };
+            if(!$breach){
+                switch($ops){
+                    case "sum":
+                        $value=$num1+$num2;
+                        echo $value;
+                        break;
+                    case "diff":
+                        $value=$num1-$num2;
+                        echo $value;
+                        break;
+                    case "prd":
+                        $value=$num1*$num2;
+                        echo $value;
+                        break;
+                    case "div":
+                        $value=$num1/$num2;
+                        echo $value;
+                        break;
+                    case "mod":
+                        $value=$num1%$num2;
+                        echo $value;
+                        break;
+                    case "exp":
+                        $value=$num1**$num2;
+                        echo $value;
+                        break;
+                    case "abs-sum":
+                        $value=abs($num1+$num2);
+                        echo "|".$value."|";
+                        break;
+                    case "abs-diff":
+                        $value=abs($num1-$num2);
+                        echo "|".$value."|";
+                        break;
+                    case "rad":
+                        $value=$num1*sqrt($num2);
+                        echo $value;
+                        break;
+                    case "prob":
+                        $value=rand($num1,$num2);
+                        echo $value;
+                        break;
+                    default:
+                        echo "OMGWTFBBQ";
+                        break;
+
+                };
+            };  
         };
-        //IT WORKS!
         ?>
     </body>
 </html>
